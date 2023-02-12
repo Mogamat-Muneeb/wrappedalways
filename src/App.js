234fd98755bdd7b1687fac64bc9b08c1f5f41b7a
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-02-12 19:11:03 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -37,10 +37,18 @@ function App() {
       <div className="flex  justify-center items-center  px-3 md:px-0">
         {!token ? (
           <div className="flex flex-col gap-3 justify-center items-center text-center">
-            <p className=""><span className="font-bold text-2xl">See your Spotify Wrapped before the end of the year!!</span>  <br/> <span className="text-[16px] font-normal">Your top tracks, artists and genres all in one place. </span> </p>
+            <p className="">
+              <span className="font-bold text-2xl">
+                See your Spotify Wrapped before the end of the year!!
+              </span>
+              <br />
+              <span className="text-[16px] font-normal">
+                Your top tracks, artists and genres all in one place.
+              </span>
+            </p>
             <a
               href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
-            className="bg-[#22c55e] py-3 px-4 rounded-3xl text-center text-white shadow-xl text-[16px] "
+              className="bg-[#22c55e] py-3 px-4 rounded-3xl text-center text-white shadow-xl text-[16px] "
             >
               Login with Spotify
             </a>
