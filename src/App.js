e4f3c8dbde845fac4ffea48e01b0d151d117323d
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-02-12 18:28:37 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const CLIENT_ID = "c97efa103ad84e6fb815ec62e02fd446";
+  const REDIRECT_URI = "http://localhost:3000/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+  }, []);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="flex flex-col  justify-center items-center">
+      <div className="text-red-400">
+      {!token ? (
+    <h1>Welcome </h1>
+      ) : (
+      <h1>You have logged in successfully!!</h1>
+      )}
+          </div>
+      {!token ? (
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
         >
-          Learn React
+          Login With Spotify
         </a>
-      </header>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 }
