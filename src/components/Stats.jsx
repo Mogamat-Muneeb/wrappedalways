18b214e730b1d0c13f0359625ba666e6af804d64
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-02-25 22:58:59 +0200

    wip

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -144,15 +144,15 @@ const Stats = (props) => {
 
   return (
     <div className="flex items-center justify-center h-full mt-10 ">
-        <div className="flex w-full  max-w-[1220px] mx-auto justify-between z-40  h-14 shadow-sm  fixed md:top-12  top-24 bg-white items-center px-2 rounded-md ">
+        <div className="md:flex hidden w-full  max-w-[1220px] mx-auto justify-center md:gap-0 gap-2 z-40  h-14 shadow-sm  fixed md:top-12  top-24 bg-white items-center px-2 rounded-md ">
           <div
-            className={`cursor-pointer md:text-[20px] text-[16px] font-semibold bg-transparent flex items-center w-full h-14   ${showing === "Top Genres" && "font-bold text-[#22c55e] transition-all duration-150 "}`}
+            className={`cursor-pointer md:text-[20px] text-[14px] font-semibold bg-transparent flex items-center w-full h-14   ${showing === "Top Genres" && "font-bold text-[#22c55e] transition-all duration-150 "}`}
             onClick={() => setShowing("Top Genres")}
           >
            Top  Genres
           </div>
           <div
-            className={`cursor-pointer  md:text-[20px] text-[16px] font-semibold bg-transparent w-full  flex items-center  h-14 ${
+            className={`cursor-pointer  md:text-[20px] text-[14px] font-semibold bg-transparent w-full  flex items-center  h-14 ${
               showing === "Top Artists" && "font-bold text-[#22c55e] transition-all duration-150"
             }`}
             onClick={() => setShowing("Top Artists")}
@@ -160,13 +160,43 @@ const Stats = (props) => {
            Top  Artists
           </div>
           <div
-            className={`cursor-pointer  md:text-[20px] text-[16px] font-semibold bg-transparent w-full  flex items-center h-14 ${showing === "Top Tracks" && "font-bold text-[#22c55e] transition-all duration-150"}`}
+            className={`cursor-pointer  md:text-[20px] text-[14px] font-semibold bg-transparent w-full  flex items-center h-14 ${showing === "Top Tracks" && "font-bold text-[#22c55e] transition-all duration-150"}`}
             onClick={() => setShowing("Top Tracks")}
           >
             Top Tracks
           </div>
           <div
-            className={`cursor-pointer  md:text-[20px] text-[16px] font-semibold bg-transparent w-full  flex items-center h-14 ${
+            className={`cursor-pointer  md:text-[20px] text-[14px] font-semibold bg-transparent w-full  flex items-center h-14 ${
+              showing === "Top Playlists" && "font-bold text-[#22c55e] transition-all duration-150"
+            }`}
+            onClick={() => setShowing("Top Playlists")}
+          >
+            Playlists
+          </div>
+        </div>
+        <div className="fixed z-40 flex items-center justify-center w-full gap-2 px-1 bg-white rounded-md shadow-sm md:hidden md:gap-0 h-14 md:top-12 top-24 ">
+          <div
+            className={`cursor-pointer  text-[14px] font-medium bg-transparent flex items-center  h-14   ${showing === "Top Genres" && "font-bold text-[#22c55e] transition-all duration-150 "}`}
+            onClick={() => setShowing("Top Genres")}
+          >
+           Top  Genres
+          </div>
+          <div
+            className={`cursor-pointer  text-[14px]  font-medium bg-transparent   flex items-center  h-14 ${
+              showing === "Top Artists" && "font-bold text-[#22c55e] transition-all duration-150"
+            }`}
+            onClick={() => setShowing("Top Artists")}
+          >
+           Top  Artists
+          </div>
+          <div
+            className={`cursor-pointer   text-[14px]  font-medium bg-transparent  flex items-center h-14 ${showing === "Top Tracks" && "font-bold text-[#22c55e] transition-all duration-150"}`}
+            onClick={() => setShowing("Top Tracks")}
+          >
+            Top Tracks
+          </div>
+          <div
+            className={`cursor-pointer   text-[14px]  font-medium bg-transparent   flex items-center h-14 ${
               showing === "Top Playlists" && "font-bold text-[#22c55e] transition-all duration-150"
             }`}
             onClick={() => setShowing("Top Playlists")}
--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -82,52 +82,6 @@ export default function Navbar(props) {
               </div>
             </div>
           </div>
-          {/* {location.pathname === "/stats" && (
-            <div className="md:flex  hidden justify-between text-white w-full h-full pt-32 pb-20  bg-[#f3f4f6] shadow-xl items-center">
-              <div className="max-w-[1285px] mx-auto flex justify-between w-full">
-                <div className="flex items-end w-full gap-10 ">
-                  <img
-                    src={userData.images[0]?.url}
-                    alt="Profile"
-                    className="w-40 h-40 border-white rounded-full border-[3px] bg-[url('https://i.postimg.cc/MGrqp8xj/Group-5.jpg)] "
-                  />
-                  <img src="https://i.postimg.cc/MGrqp8xj/Group-5.jpg" alt="" />
-                  <p className="text-3xl font-extrabold bg-transparent">
-                    {userData.display_name}
-                  </p>
-                </div>
-                <div className="flex flex-col items-end justify-start ">
-                  <div className="flex gap-2 hover:text-[#22c55e] font-bold w-full bg-transparent">
-                    <Link
-                      target="_blank"
-                      className="hover:text-[#22c55e] flex gap-2 bg-transparent "
-                      to={`https://open.spotify.com/user/${userData.id}`}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        class="w-6 h-6 fill-current bg-transparent"
-                      >
-                        <g>
-                          <path fill="none" d="M0 0h24v24H0z"></path>
-                          <path
-                            fill-rule="nonzero"
-                            d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.55 2 12 2zm3.75 14.65c-2.35-1.45-5.3-1.75-8.8-.95-.35.1-.65-.15-.75-.45-.1-.35.15-.65.45-.75 3.8-.85 7.1-.5 9.7 1.1.35.15.4.55.25.85-.2.3-.55.4-.85.2zm1-2.7c-2.7-1.65-6.8-2.15-9.95-1.15-.4.1-.85-.1-.95-.5-.1-.4.1-.85.5-.95 3.65-1.1 8.15-.55 11.25 1.35.3.15.45.65.2 1s-.7.5-1.05.25zM6.3 9.75c-.5.15-1-.15-1.15-.6-.15-.5.15-1 .6-1.15 3.55-1.05 9.4-.85 13.1 1.35.45.25.6.85.35 1.3-.25.35-.85.5-1.3.25C14.7 9 9.35 8.8 6.3 9.75z"
-                          ></path>
-                        </g>
-                      </svg>
-                      Logo spotify open in Spotiy
-                    </Link>
-                  </div>
-                  <div className="bg-transparent">
-                    <p className="bg-transparent">
-                      {userData.followers.total} Followers
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          )} */}
         </>
       ) : null}
       {props.token ? (
@@ -137,11 +91,13 @@ export default function Navbar(props) {
               <Link to="/" className="flex items-center justify-start w-full">
               <LogoIcon/>
               </Link>
-              <img
-                    src={userData.images[0]?.url}
-                    alt="Profile"
-                    className="w-10 h-10 shadow border-white rounded-full border-[3px] bg-[url('https://i.postimg.cc/MGrqp8xj/Group-5.jpg)] "
-                  />
+              {userData.images[0]?.url ? (
+                <img
+                      src={userData.images[0]?.url}
+                      alt="Profile"
+                      className="w-10 h-10 shadow border-white rounded-full border-[3px] bg-[url('https://i.postimg.cc/MGrqp8xj/Group-5.jpg)] "
+                    />
+              ): (null) }
               <div
                 className="flex items-center justify-end w-full"
               >
